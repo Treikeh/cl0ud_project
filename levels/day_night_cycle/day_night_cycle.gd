426bf436f8a2dc9e389e90ad578d454d91a0aadd
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-04-13 10:24:27 +0200

    Updated day night cycle to also change the color of the skybox.

--- levels/day_night_cycle/day_night_cycle.gd
+++ levels/day_night_cycle/day_night_cycle.gd
@@ -2,10 +2,18 @@ extends Node3D
 
 
 @export var _speed: float = 10.0
+
+@export_group("World environment")
 @export var _world_environment: WorldEnvironment
+@export var _sky_top_color: GradientTexture1D
+@export var _sky_horizon_color: GradientTexture1D
+@export var _ground_horizon_color: GradientTexture1D
+@export var _ground_bottom_color: GradientTexture1D
+
+@export_group("Sky light")
 @export var _directional_light: DirectionalLight3D
-@export var _curve: Curve
-@export var _color_gradient: GradientTexture1D
+@export var _sky_light_curve: Curve
+@export var _sky_light_color: GradientTexture1D
 
 
 func _process(delta: float) -> void:
@@ -17,13 +25,24 @@ func _process(delta: float) -> void:
 	if _directional_light.rotation_degrees.x < -180.0:
 		_directional_light.rotation_degrees.x = 179.0
 	
+	
+	var color_sample: int = int(_directional_light.rotation_degrees.x + 180.0)
+	var color_sample_remap: float = remap(color_sample, 0.0, 360.0, 0.0, 1.0)
+	
+	# Change sky color
+	var sky_mat: ProceduralSkyMaterial = _world_environment.environment.sky.sky_material
+	sky_mat.sky_top_color = _sky_top_color.gradient.sample(color_sample_remap)
+	sky_mat.sky_horizon_color = _sky_horizon_color.gradient.sample(color_sample_remap)
+	sky_mat.ground_horizon_color = _ground_horizon_color.gradient.sample(color_sample_remap)
+	sky_mat.ground_bottom_color = _ground_bottom_color.gradient.sample(color_sample_remap)
+	
+	
 	# Change light intensity
-	var sample: float = _directional_light.rotation_degrees.x
-	_directional_light.light_energy = _curve.sample(sample)
+	var curve_sample: float = _directional_light.rotation_degrees.x
+	_directional_light.light_energy = _sky_light_curve.sample(curve_sample)
 	
 	# Change light color 
-	var color_sample: int = int(_directional_light.rotation_degrees.x + 180.0)
-	var color: Color = _color_gradient.gradient.sample(remap(color_sample, 0.0, 360.0, 0.0, 1.0))
+	var color: Color = _sky_light_color.gradient.sample(color_sample_remap)
 	_directional_light.light_color = color
 
 
