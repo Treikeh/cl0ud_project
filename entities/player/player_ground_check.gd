0b318451bcefe1de101b915bb7269ed69d1904fe
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-04-27 13:41:54 +0200

    Added distance label to the fishing rod

--- entities/player/player_ground_check.gd
+++ entities/player/player_ground_check.gd
@@ -14,13 +14,10 @@ var ground_normal: Vector3 = Vector3.UP
 var _gravity_dir: Vector3
 var _player: Player
 
-@onready var _ray_cast_3d: RayCast3D = get_child(0)
-
 
 func setup(player: Player, gravity_dir: Vector3) -> void:
 	_player = player
 	_gravity_dir = gravity_dir
-	_ray_cast_3d.collision_mask = collision_mask
 
 
 func is_grounded() -> bool:
@@ -29,11 +26,7 @@ func is_grounded() -> bool:
 		return false
 	
 	if is_colliding():
-		# Move RayCast to ShapeCast collision point on the XZ plane (don't change height)
-		var col_pos: Vector3 = get_collision_point(0)
-		_ray_cast_3d.global_position.x = col_pos.x
-		_ray_cast_3d.global_position.z = col_pos.z
-		ground_normal = _ray_cast_3d.get_collision_normal()
+		ground_normal = get_collision_normal(0)
 		# Compare ground normal to upwards direction to get the slope angle
 		if ground_normal.angle_to(Vector3.UP) < deg_to_rad(_max_slope_angle):
 			return true
@@ -43,11 +36,7 @@ func is_grounded() -> bool:
 
 # Apply a spring force that moves the palyer towards "rest_height"
 func snap_to_ground() -> void:
-	# Check if ray cast is hitting the floor
-	if not _ray_cast_3d.is_colliding():
-		return
-	
-	var hit_distance: float = (global_position - _ray_cast_3d.get_collision_point()).length()
+	var hit_distance: float = (global_position - get_collision_point(0)).length()
 	var normal_vel: float = -ground_normal.dot(_player.linear_velocity)
 	var dispalcement: float = hit_distance - _rest_height
 	var force: float = (_spring_force * dispalcement) - (normal_vel * _spring_damping)
