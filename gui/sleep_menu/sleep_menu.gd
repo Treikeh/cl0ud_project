e2c8fd1547e6e7849a183e758e41e237ce5f47c0
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-07 11:58:38 +0200

    Added a sun dial to the sleep menu

--- gui/sleep_menu/sleep_menu.gd
+++ gui/sleep_menu/sleep_menu.gd
@@ -8,12 +8,15 @@ const TWEEN_TIME: float = 1.0
 
 @export var _label: Label
 var _time_of_day: float
+
+@onready var sun_dial: Control = $Control/SunDailRoot/SunDial
 @onready var _control_root: Control = $Control
 
 
 func _ready() -> void:
 	_time_of_day = Globals.time_of_day
 	_control_root.modulate = Color.TRANSPARENT
+	
 	
 	await _fade_inn()
 	
@@ -25,7 +28,12 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	_label.text = str(_time_of_day)
+	var label_time: float = _time_of_day
+	if label_time > 24.0:
+		label_time -= 24.0
+	_label.text = str(snappedf(label_time, 0.1))
+	
+	sun_dial.rotation_degrees = _time_of_day * 15.0
 
 
 func _fade_inn() -> void:
@@ -42,7 +50,11 @@ func _fade_out() -> void:
 
 
 func _change_time_dial() -> void:
+	const START_TIME: float = 6.0
+	var hours_until_midnight: float = 24.0 - _time_of_day
+	var target_time: float = _time_of_day + hours_until_midnight + START_TIME
+	
 	var tween: Tween = create_tween()
-	tween.tween_property(self, "_time_of_day", 6.0, TWEEN_TIME)
+	tween.tween_property(self, "_time_of_day", target_time, TWEEN_TIME)
 	tween.tween_interval(0.5)
 	await tween.finished
