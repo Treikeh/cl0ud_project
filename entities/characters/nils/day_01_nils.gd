68caeb487d4334ae4f4e4c80dbf626cd2bfaf7e2
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-08 12:51:00 +0200

    Added more dialogue

--- entities/characters/nils/day_01_nils.gd
+++ entities/characters/nils/day_01_nils.gd
@@ -6,16 +6,32 @@ extends Node3D
 @export_group("Dialogue")
 @export var _start_dialogue: DialogueData
 @export var _spoken_dialogue: DialogueData
+@export var _teach_fishing_dialogue: DialogueData
+@export var _repeat_dialogue: DialogueData
+@export var _fishing_rod_upgrade: Upgrade
 
 var _times_spoken: int = 0
+var _caught_fish: bool = false
+
+
+func _ready() -> void:
+	Globals.fish_caught.connect(func(): _caught_fish = true)
 
 
 func _on_interacted(player: Player) -> void:
+	if player.upgrades.has(_fishing_rod_upgrade):
+		if _caught_fish:
+			player.hud.start_dialogue(_repeat_dialogue)
+		else:
+			player.hud.start_dialogue(_teach_fishing_dialogue)
+		return
+	
 	match _times_spoken:
 		# First time we speak to nils
-		0: player.hud.start_dialogue(_start_dialogue)
+		0: 
+			player.hud.start_dialogue(_start_dialogue)
+			_times_spoken += 1
 		# Every other time we speak to nils
 		_: player.hud.start_dialogue(_spoken_dialogue)
 	
-	_times_spoken += 1
 	player.update_look_position(_head_maker.global_position)
