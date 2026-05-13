a9d99d44e56e7aa0626b97140c22d297a330cf0d
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-13 11:15:04 +0200

    Made the profile wall load all the profiles when it loads

--- entities/profile_wall/profile_wall.gd
+++ entities/profile_wall/profile_wall.gd
@@ -2,6 +2,11 @@ extends Node3D
 
 
 const PROFILE_MENU_SCENE: PackedScene = preload("uid://cg2ukppievp2t")
+var _profiles: Array[ProfileData] = []
+
+
+func _ready() -> void:
+	_load_profiles()
 
 
 func _on_interacted(player: Player) -> void:
@@ -9,7 +14,7 @@ func _on_interacted(player: Player) -> void:
 	
 	var inv: Inventory = player.inventory
 	var piece_inv: Inventory = player.piece_inventory
-	var profile_menu: Control = PROFILE_MENU_SCENE.instantiate().with_data(inv, piece_inv)
+	var profile_menu: Control = PROFILE_MENU_SCENE.instantiate().with_data(inv, piece_inv, _profiles)
 	add_child(profile_menu)
 	profile_menu.closed.connect(_on_menu_closed.bind(player))
 
@@ -17,3 +22,12 @@ func _on_interacted(player: Player) -> void:
 func _on_menu_closed(player: Player) -> void:
 	player.input.set_enabled.call_deferred(true)
 	player.update_look_position(Vector3.ZERO)
+
+
+func _load_profiles() -> void:
+	const PATH: String = "res://common/data/profiles/"
+	var files: PackedStringArray = ResourceLoader.list_directory(PATH)
+	for file: String in files:
+		if file.ends_with(".tres"):
+			var profile: ProfileData = load(PATH + file)
+			_profiles.append(profile)
