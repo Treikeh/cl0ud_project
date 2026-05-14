6efef3f71da6a4ffd967fe209d454d920daa4535
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-14 12:41:56 +0200

    Added select marker to inventory slot and added info about selected item in the inventory

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -179,7 +179,7 @@ func _on_inventory_opened() -> void:
 	input.set_enabled(false)
 	
 	# Spawn inventory
-	_inventory_menu = INVENTORY_SCENE.instantiate().with_data(inventory, Globals.upgrades)
+	_inventory_menu = INVENTORY_SCENE.instantiate().with_data(inventory)
 	add_child(_inventory_menu)
 	_inventory_menu.closed.connect(_on_inventory_closed)
 
