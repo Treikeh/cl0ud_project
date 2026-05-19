908df733ce46121a219dbed0d8b3ddfe18ffdcae
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-19 13:54:09 +0200

    Added tips to old man repeat dialogue

--- entities/characters/nils/day_01_nils.gd
+++ entities/characters/nils/day_01_nils.gd
@@ -8,7 +8,8 @@ extends Node3D
 @export var _chops_dialogue: DialogueData
 @export var _spoken_dialogue: DialogueData
 @export var _teach_fishing_dialogue: DialogueData
-@export var _repeat_dialogue: DialogueData
+@export var _repeat_dialogues: Array[DialogueData]
+@export var _tips_dialogues: Array[DialogueData]
 @export var _fishing_rod_upgrade: Upgrade
 
 var _times_spoken: int = 0
@@ -19,6 +20,16 @@ func _ready() -> void:
 	Globals.fish_caught.connect(func(): _caught_fish = true)
 	
 	_start_dialogue.choice_made.connect(_on_start_dialogue_choice_made)
+	
+	for dialogue: DialogueData in _repeat_dialogues:
+		dialogue.choice_made.connect(_on_repeat_dialogue_choice_made)
+
+
+func _on_repeat_dialogue_choice_made(choice: int) -> void:
+	var player: Player = get_tree().get_first_node_in_group("player")
+	match choice:
+		_:
+			player.hud.start_dialogue.call_deferred(_tips_dialogues.pick_random())
 
 
 func _on_start_dialogue_choice_made(choice: int) -> void:
@@ -31,7 +42,7 @@ func _on_start_dialogue_choice_made(choice: int) -> void:
 func _on_interacted(player: Player) -> void:
 	if Globals.upgrades.has(_fishing_rod_upgrade):
 		if _caught_fish:
-			player.hud.start_dialogue(_repeat_dialogue)
+			player.hud.start_dialogue(_repeat_dialogues.pick_random())
 		else:
 			player.hud.start_dialogue(_teach_fishing_dialogue)
 		return
