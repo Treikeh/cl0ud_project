8f872d386aa36e67e6da82e5100b303b1c853912
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-19 10:47:59 +0200

    Updated dialogue

--- entities/characters/nils/day_01_nils.gd
+++ entities/characters/nils/day_01_nils.gd
@@ -5,6 +5,7 @@ extends Node3D
 
 @export_group("Dialogue")
 @export var _start_dialogue: DialogueData
+@export var _chops_dialogue: DialogueData
 @export var _spoken_dialogue: DialogueData
 @export var _teach_fishing_dialogue: DialogueData
 @export var _repeat_dialogue: DialogueData
@@ -16,6 +17,15 @@ var _caught_fish: bool = false
 
 func _ready() -> void:
 	Globals.fish_caught.connect(func(): _caught_fish = true)
+	
+	_start_dialogue.choice_made.connect(_on_start_dialogue_choice_made)
+
+
+func _on_start_dialogue_choice_made(choice: int) -> void:
+	var player: Player = get_tree().get_first_node_in_group("player")
+	match choice:
+		_:
+			player.hud.start_dialogue.call_deferred(_chops_dialogue)
 
 
 func _on_interacted(player: Player) -> void:
