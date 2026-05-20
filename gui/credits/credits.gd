97729288bf7d5b97ccf068cf25bc6179439fce0e
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-20 13:37:43 +0200

    Added a simple credits screen

--- gui/credits/credits.gd
+++ gui/credits/credits.gd
@@ -1,1 +1,11 @@
 extends Control
+
+
+func _on_meta_clicked(meta):
+	# `meta` is not guaranteed to be a String, so convert it to a String
+	# to avoid script errors at runtime.
+	OS.shell_open(str(meta))
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
