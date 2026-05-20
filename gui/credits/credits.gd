7874a9ba872375d1ff73111ddd619ba479c1377e
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-20 14:10:18 +0200

    Created a ending version of the old man and set up fade to black for the credits.

--- gui/credits/credits.gd
+++ gui/credits/credits.gd
@@ -1,6 +1,15 @@
 extends Control
 
 
+@export var _fade_thing: Control
+
+
+func _ready() -> void:
+	_fade_thing.modulate = Color.WHITE
+	var tween: Tween = create_tween()
+	tween.tween_property(_fade_thing, "modulate", Color.TRANSPARENT, 3.0)
+
+
 func _on_meta_clicked(meta):
 	# `meta` is not guaranteed to be a String, so convert it to a String
 	# to avoid script errors at runtime.
