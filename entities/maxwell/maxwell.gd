d79401d32e16263a45f12333789b40a4c5e85692
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-21 11:42:52 +0200

    Added fish to the fish tank

--- entities/maxwell/maxwell.gd
+++ entities/maxwell/maxwell.gd
@@ -6,8 +6,8 @@ extends Node3D
 
 
 func _on_interact_area_3d_interacted(player: Player) -> void:
-	#if player.fishing_rod._hook.hooked_fish != null:
-	#	player.fishing_rod._hook.hooked_fish.queue_free()
+	if player.fishing_rod._hook.hooked_fish != null:
+		player.fishing_rod._hook.hooked_fish.queue_free()
 	
 	if not _anim.is_playing():
 		_anim.play("pet")
