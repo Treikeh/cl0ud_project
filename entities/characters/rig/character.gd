7cd3ef04ba5fbed10bb93d4c5f64a87062d6e69c
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-05-22 13:12:38 +0200

    Updated characters texture and added a character slot for a version of the old man with a gas mask.

--- entities/characters/rig/character.gd
+++ entities/characters/rig/character.gd
@@ -7,7 +7,7 @@ class_name Character
 @export var _default_look_position: Node3D
 
 @export_group("Skinning")
-@export_enum("Base", "Vendor", "Nils", "Fisher01", "Fisher02", "Fisher03") var _skin: String = "Base"
+@export_enum("Base", "Vendor", "Nils", "Fisher01", "Fisher02", "Fisher03", "NilsCloudMask") var _skin: String = "Base"
 @export var _animation: AnimationRootNode
 
 @onready var _animation_tree: AnimationTree = $AnimationTree
